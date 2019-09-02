0c44b71eabe3af75ed0e1155977b1c2b46514de4
alicebyers5 <alice.byers@nhs.net>
2019-09-02 12:29:36 +0100

    Merge SMR01 and 50 queries

--- code/01_create-data.R
+++ code/01_create-data.R
@@ -31,6 +31,7 @@ smra_connect <-
 
 ### 3 - Extract data ----
 
+
 deaths <- 
   as_tibble(dbGetQuery(smra_connect, 
                        deaths_query(start_date,
@@ -38,21 +39,25 @@ deaths <-
                                     external))) %>% 
   clean_names()
 
+
 smr01 <- 
+  
   as_tibble(dbGetQuery(smra_connect, 
                        smr01_query(start_date,
                                    end_date,
                                    smr_start_date,
-                                   external))) %>% 
+                                   external,
+                                   gls = FALSE))) %>% 
+  
+  bind_rows(as_tibble(dbGetQuery(smra_connect, 
+                                 smr01_query(start_date,
+                                             end_date,
+                                             smr_start_date,
+                                             external,
+                                             gls = TRUE)))) %>%
+  
   clean_names()
 
-smr50 <- 
-  as_tibble(dbGetQuery(smra_connect, 
-                       smr50_query(start_date,
-                                   end_date,
-                                   smr_start_date,
-                                   external))) %>% 
-  clean_names()
 
 smr04 <- 
   as_tibble(dbGetQuery(smra_connect, 
