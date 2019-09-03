9f2e1dcf09a114fe6197c073fc98fee9fb3716f6
alicebyers5 <alice.byers@nhs.net>
2019-09-03 09:37:27 +0100

    Aggregate SMR data to CIS level and join

--- code/01_create-data.R
+++ code/01_create-data.R
@@ -31,7 +31,6 @@ smra_connect <-
 
 ### 3 - Extract data ----
 
-
 deaths <- 
   as_tibble(dbGetQuery(smra_connect, 
                        deaths_query(extract_start = start_date,
@@ -66,6 +65,34 @@ smr04 <-
                                    extract_start_smr = smr_start_date,
                                    external_causes = external))) %>% 
   clean_names()
+
+
+### 4 - Aggregate SMR data to CIS level and join ----
+
+smr01 %<>%
+  group_by(link_no, gls_cis_marker) %>%
+  summarise(admission_date = min(admission_date),
+            discharge_date = max(discharge_date),
+            date_of_death  = max(date_of_death)) %>%
+  ungroup() %>%
+  select(-gls_cis_marker)
+
+smr04 %<>%
+  
+  # Use date of death as proxy where discharge date is missing
+  mutate(discharge_date = if_else(is.na(discharge_date), 
+                                  date_of_death, 
+                                  discharge_date)) %>%
+  
+  group_by(link_no, cis_marker) %>%
+  summarise(admission_date = min(admission_date),
+            discharge_date = max(discharge_date),
+            date_of_death  = max(date_of_death)) %>%
+  ungroup() %>%
+  select(-cis_marker)
+  
+smr <-
+  bind_rows(smr01, smr04)
 
 
 ### END OF SCRIPT ###
