0584b7be33e5518c3f4d05818f0d7a811fa7f60d
alicebyers5 <alice.byers@nhs.net>
2019-09-18 11:11:12 +0100

    Setup script to create figures for summary and report

---
+++ functions/02_create-figures.R
@@ -0,0 +1,13 @@
+#########################################################################
+# Name of file - 02_create-figures.R
+# Data release - End of Life Publication
+# Original Authors - Alice Byers
+# Orginal Date - September 2019
+#
+# Written/run on - RStudio Server
+# Version of R - 3.6.1
+#
+# Description - Create and save figures for summary and report.
+#
+# Approximate run time - xx minutes
+#########################################################################
