6fb77a1cb081448a926b3fe461b314282a9dbd95
alicebyers5 <alice.byers@nhs.net>
2019-09-18 11:30:54 +0100

    Create figure 1

--- code/02_create-figures.R
+++ code/02_create-figures.R
@@ -11,3 +11,40 @@
 #
 # Approximate run time - xx minutes
 #########################################################################
+
+
+### 1 - Setup environment and load functions ----
+
+source(here::here("code", "00_setup-environment.R"))
+source(here::here("functions", "summarise_data.R"))
+
+
+### 2 - Read in basefile ----
+
+basefile <- read_rds(here("data", "basefiles", 
+                          glue("{pub_date}_base-file.rds")))
+
+
+### 3 - Figure 1 ----
+
+fig1 <- 
+  
+  summarise_data(basefile, trend = TRUE) %>%
+  select(-deaths, -comm) %>%
+  mutate(qom_comm = 100 - qom) %>%
+  pivot_longer(cols = qom:qom_comm,
+               names_to = "qom") %>%
+  mutate(qom = if_else(qom == "qom",
+                       "Hopsital",
+                       "Home/Community")) %>%
+  
+  ggplot(aes(x = fy, y = value, fill = qom)) +
+  geom_bar(position = "stack", stat = "identity", width = 0.5, show.legend = T) +
+  theme(panel.background = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_blank(),
+        axis.title.x = element_text(size = 12, face = "bold"),
+        axis.title.y = element_text(size = 12, face = "bold"),
+        axis.text = element_text(size = 12)) +
+  xlab("Financial Year of Death") + 
+  ylab("Percentage")
