c6757117a3b226f0282bed457b892f53dc4e7498
alicebyers5 <alice.byers@nhs.net>
2019-09-23 16:47:11 +0100

    Make changes to calculate_qom function

--- functions/calculate_qom.R
+++ functions/calculate_qom.R
@@ -13,28 +13,18 @@
 #########################################################################
 
 
-calculate_qom <- function(data, setting){
+calculate_qom <- function(los, deaths, setting){
   
   if(!(setting %in% c("hosp", "comm"))){
     stop("The setting argument must be either 'hosp' or 'comm'.")
   }
-  
-  if(!("los" %in% names(data))){
-    stop("The given data must include variable named 'los'.")
-  }
-  
-  if(!("deaths" %in% names(data))){
-    stop("The given data must include variable named 'deaths'.")
-  }
-  
-  los <- sum(data$los)
-  deaths <- sum(data$deaths)
   
   if_else(setting == "hosp",
           ((los / deaths) / 182.5) * 100,
           100 - ((los / deaths) / 182.5) * 100) %>%
     
   janitor::round_half_up(1)
+  
   
 }
 
