406e90616a902ba5e48d51d94475b7d58336963e
alicebyers5 <alice.byers@nhs.net>
2019-11-01 15:25:15 +0000

    Add annotations to map

--- code/02_create-figures.R
+++ code/02_create-figures.R
@@ -100,7 +100,19 @@ fig2 <-
         axis.text = element_blank(),
         axis.title = element_blank(),
         axis.ticks = element_blank(),
-        legend.title = element_blank())
+        legend.title = element_blank()) +
+  annotate("text", 
+           x = 4e+05, y = 620000, 
+           label = paste0("NHS ", unique(fig2$HBName[which.min(fig2$qom)]),
+                          ": ", min(round_half_up(fig2$qom, 1)), "%"), 
+           size = 3.5,
+           fontface = 2) +
+  annotate("text", 
+           x = 4e+05, y = 1100000, 
+           label = paste0("NHS ", unique(fig2$HBName[which.max(fig2$qom)]),
+                          ": ", max(round_half_up(fig2$qom, 1)), "%"), 
+           size = 3.5,
+           fontface = 2)
 
 ggsave(here("markdown", "figures", "figure-2.png"), 
        plot = fig2,
