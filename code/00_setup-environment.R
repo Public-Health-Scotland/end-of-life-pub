9a40054eab9231139d966a2460c7386fcbbff3cd
fraserstirrat <fraser.stirrat@phs.scot>
2025-08-27 11:16:11 +0000

    Updated the file path to conf/linkage rather than stats/cl-out
    Updated the lookup for Urban Rural from 2020 to 2022

--- code/00_setup-environment.R
+++ code/00_setup-environment.R
@@ -63,7 +63,7 @@ if (sessionInfo()$platform %in% c("x86_64-redhat-linux-gnu (64-bit)",
 # locally or on server
 filepath <- dplyr::if_else(platform == "server",
                            "/conf/linkage/output/",
-                           "//stats/cl-out/")
+                           "/conf/linkage/output/")
 
 
 ### 3 - Define dates ----
@@ -158,8 +158,8 @@ postcode <- function(version =""){
   clean_names() %>%
   
   select(pc7, ca2019, ca2019name, ca2018, hb2019, hb2019name,
-         hscp2019, hscp2019name, hscp2018, ur6_2020_name, 
-         ur2_2020_name, datazone2011) %>%
+         hscp2019, hscp2019name, hscp2018, ur6_2022_name, 
+         ur2_2022_name, datazone2011) %>%
     
   rename(hb = hb2019name,
          hbcode = hb2019,
@@ -167,8 +167,8 @@ postcode <- function(version =""){
          hscpcode = hscp2019,
          ca = ca2019name,
          cacode = ca2019,
-         urban_rural = ur6_2020_name,
-         urban_rural_2 = ur2_2020_name)
+         urban_rural = ur6_2022_name,
+         urban_rural_2 = ur2_2022_name)
 
 }
             
